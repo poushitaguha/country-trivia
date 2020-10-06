a50ad2d062afdd022d9b57ae75536c5b3be8cf82
poushitaguha <pguha.kolkata@gmail.com>
2020-10-06 15:00:18 -0400

    Add logo, install Axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import logo from './images/trivia-logo.png';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <header>
+        <h1 className='sr-only'>Country Trivia</h1>
+        <img src={logo} alt='Logo for Qurantine Pursuit' />
       </header>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

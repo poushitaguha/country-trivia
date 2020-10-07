fa46e05dad671f68addbe21b8e39888fca2feb91
poushitaguha <pguha.kolkata@gmail.com>
2020-10-07 10:42:26 -0400

    Add contents in Quiz component

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,41 @@ const app = express();
 const request = require('request');
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// app.get('/countrydetails', (req, res) => {
+//   request(
+//     'https://restcountries-v1.p.rapidapi.com/all?rapidapi-key=b8affda141mshd5ab74ebd5087c7p1abc68jsn209947cdf239',
+//     function(error, response, body) {
+//       if (!error && response.statusCode == 200) {
+//         res.send(body);
+//         // var parsedData = JSON.parse(body);
+//         // var temp_c = parsedData.main.temp;
+//         // res.send({ temp_c });
+//       }
+//     }
+//   );
+// });
+
+// app.get('/countryquiz', (req, res) => {
+//   request(
+//     'https://restcountries-v1.p.rapidapi.com/all?rapidapi-key=b8affda141mshd5ab74ebd5087c7p1abc68jsn209947cdf239',
+//     function(error, response, body) {
+//       if (!error && response.statusCode === 200) {
+//         // res.send(body);
+//         var parsedData = JSON.parse(body);
+//         // var temp_c = parsedData.main.temp;
+//         // res.send({ temp_c });
+//         console.log(parsedData);
+//         for (let i = 0; i < parsedData.length; i++) {
+//           console.log(parsedData[i]);
+//           // Get the Country and Capital from parsed JSON data
+//           // var name = parsedData[i].name;
+//           var capital = parsedData[i].capital;
+//           var currency = parsedData[i].currencies;
+//         }
+//       }
+//     }
+//   );
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- frontend/src/components/CountryQuiz.js
+++ frontend/src/components/CountryQuiz.js
@@ -4,11 +4,67 @@ import axios from 'axios';
 class CountryQuiz extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      name: '',
+      capital: '',
+      currency: '',
+      continent: ''
+    };
   }
 
+  // componentDidMount = () => {
+  //   axios.get('countryquiz').then(response => {
+  //     console.log(response);
+  //     this.setState({
+  //       name: response.data.name,
+  //       capital: response.data.capital,
+  //       currency: response.data.currency,
+  //       continent: response.data.continent
+  //     });
+  //   });
+  // };
+
   render() {
-    return <form className='centered'></form>;
+    return (
+      <form className='centered'>
+        <label htmlFor='inputCapital'>
+          What is the capital of
+          {/* {this.props.name}? */}
+        </label>
+        <input
+          className='inputCapital'
+          type='text'
+          id='inputCapital'
+          placeholder='Enter Capital'
+        />
+        {/* <button onClick={this.handleClick}>Display Weather Info</button> */}
+        <button>Submit</button>
+        <br></br>
+        <label htmlFor='inputCurrency'>
+          What is the currency of
+          {/* {this.props.name}? */}
+        </label>
+        <input
+          className='inputCurrency'
+          type='text'
+          id='inputCurrency'
+          placeholder='Enter Currency'
+        />
+        <button>Submit</button>
+        <br></br>
+        <label htmlFor='inputContinent'>
+          Which continent does
+          {/* {this.props.name} fall in?? */}
+        </label>
+        <input
+          className='inputContinent'
+          type='text'
+          id='inputContinent'
+          placeholder='Enter Continent'
+        />
+        <button>Submit</button>
+      </form>
+    );
   }
 }
 

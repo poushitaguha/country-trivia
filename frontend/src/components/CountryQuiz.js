820020b356f9ac8066e49481d0549426b618bf5b
poushitaguha <pguha.kolkata@gmail.com>
2020-10-07 23:23:13 -0400

    Fix quiz logic

--- frontend/src/components/CountryQuiz.js
+++ frontend/src/components/CountryQuiz.js
@@ -4,12 +4,15 @@ import axios from 'axios';
 class CountryQuiz extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    // console.log(props);
     this.state = {
       name: '',
       capital: '',
       currency: '',
-      region: ''
+      region: '',
+      inputCapital: '',
+      inputCurrency: '',
+      inputContinent: ''
     };
   }
 
@@ -17,7 +20,7 @@ class CountryQuiz extends Component {
     axios
       .get(`countryquiz?countryName=${this.props.countryName}`)
       .then(response => {
-        console.log(response);
+        // console.log(response);
         this.setState({
           name: response.data.name,
           capital: response.data.capital,
@@ -28,42 +31,57 @@ class CountryQuiz extends Component {
   };
 
   inputCapital = event => {
-    this.setState({ capital: event.target.value });
+    this.setState({ ...this.state, inputCapital: event.target.value });
   };
 
   inputCurrency = event => {
-    this.setState({ currency: event.target.value });
+    this.setState({ ...this.state, inputCurrency: event.target.value });
   };
 
   inputContinent = event => {
-    this.setState({ region: event.target.value });
+    this.setState({ ...this.state, inputContinent: event.target.value });
   };
 
   submitCapital = event => {
     event.preventDefault();
-    const { capital } = this.state;
-    const matches = capital === event.target.value;
-    matches
-      ? alert('This is the correct answer. Well done!!')
-      : alert(`Incorrect answer. The correct answer is ${this.state.capital}`);
+    const { capital, inputCapital } = this.state;
+
+    // console.log('capital' + capital);
+    // console.log('inputCapital' + inputCapital);
+
+    if (capital === inputCapital) {
+      alert('This is the correct answer. Well done!!');
+    } else {
+      alert(`Incorrect answer. The correct answer is ${this.state.capital}`);
+    }
   };
 
   submitCurrency = event => {
     event.preventDefault();
-    const { currency } = this.state;
-    const matches = currency === event.target.value;
-    matches
-      ? alert('This is the correct answer. Well done!!')
-      : alert(`Incorrect answer. The correct answer is ${this.state.currency}`);
+    const { currency, inputCurrency } = this.state;
+
+    // console.log('currency' + currency);
+    // console.log('inputCurrency' + inputCurrency);
+
+    if (currency === inputCurrency) {
+      alert('This is the correct answer. Well done!!');
+    } else {
+      alert(`Incorrect answer. The correct answer is ${this.state.currency}`);
+    }
   };
 
   submitContinent = event => {
     event.preventDefault();
-    const { region } = this.state;
-    const matches = region === event.target.value;
-    matches
-      ? alert('This is the correct answer. Well done!!')
-      : alert(`Incorrect answer. The correct answer is ${this.state.region}`);
+    const { region, inputContinent } = this.state;
+
+    // console.log('region' + region);
+    // console.log('inputContinent' + inputContinent);
+
+    if (region === inputContinent) {
+      alert('This is the correct answer. Well done!!');
+    } else {
+      alert(`Incorrect answer. The correct answer is ${this.state.region}`);
+    }
   };
 
   render() {
@@ -72,7 +90,6 @@ class CountryQuiz extends Component {
         <form className='centered' onSubmit={this.submitCapital}>
           <label htmlFor='inputCapital'>
             What is the capital of {this.props.countryName}?
-            {/* {this.state.capital} */}
           </label>
           <input
             className='inputCapital'
@@ -87,7 +104,6 @@ class CountryQuiz extends Component {
         <form className='centered' onSubmit={this.submitCurrency}>
           <label htmlFor='inputCurrency'>
             What is the currency of {this.props.countryName}?
-            {/* {this.state.currency} */}
           </label>
           <input
             className='inputCurrency'
@@ -102,7 +118,6 @@ class CountryQuiz extends Component {
         <form className='centered' onSubmit={this.submitContinent}>
           <label htmlFor='inputContinent'>
             Which continent does {this.props.countryName} fall in?
-            {/* {this.state.region} */}
           </label>
           <input
             className='inputContinent'

d93a2c84b4f9d30457e56f04961b79607593cb5e
poushitaguha <pguha.kolkata@gmail.com>
2020-10-08 21:34:27 -0400

    Add env and dotnet

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const request = require('request');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/countrydetails', (req, res) => {
   // Get Country Name from req
